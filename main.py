047e78dae5f162ce76684de070f553aee7906c10
dexon401 <255047233+dexon401@users.noreply.github.com>
2026-03-30 20:19:41 +0300

    Did the thing

--- main.py
+++ main.py
@@ -10,66 +10,83 @@ app.config["SECRET_KEY"] = "yandexlyceum_secret_key"
 
 def main():
     db_session.global_init("db/mars_explorer.db")
-    
-    db_sess = db_session.create_session()
-    
-    user = User()
-    user.surname = "Scott"
-    user.name = "Ridley"
-    user.age = 21
-    user.position = "captain"
-    user.speciality = "recearch engineer"
-    user.address = "module_1"
-    user.email = "scott_chief@mars.org"
-    db_sess.add(user)
-    
-    user2 = User()
-    user2.surname = "Smith"
-    user2.name = "John"
-    user2.age = 25
-    user2.position = "engineer"
-    user2.speciality = "mechanical engineer"
-    user2.address = "module_2"
-    user2.email = "john_smith@mars.org"
-    db_sess.add(user2)
-    
-    user3 = User()
-    user3.surname = "Johnson"
-    user3.name = "Jane"
-    user3.age = 28
-    user3.position = "scientist"
-    user3.speciality = "biologist"
-    user3.address = "module_3"
-    user3.email = "jane_johnson@mars.org"
-    db_sess.add(user3)
-    
-    user4 = User()
-    user4.surname = "Brown"
-    user4.name = "Bob"
-    user4.age = 30
-    user4.position = "pilot"
-    user4.speciality = "aeronautics"
-    user4.address = "module_4"
-    user4.email = "bob_brown@mars.org"
-    db_sess.add(user4)
-    
-    job = Jobs()
-    job.team_leader = 1
-    job.job = "deployment of residential modules 1 and 2"
-    job.work_size = 15
-    job.collaborators = "2, 3"
-    job.start_date = datetime.datetime.now()
-    job.is_finished = False
-    db_sess.add(job)
-    
-    db_sess.commit()
-    
-    app.run()
-    
 
-@app.route()
+    db_sess = db_session.create_session()
+
+    if db_sess.query(User).count() == 0:
+        user = User()
+        user.surname = "Scott"
+        user.name = "Ridley"
+        user.age = 21
+        user.position = "captain"
+        user.speciality = "recearch engineer"
+        user.address = "module_1"
+        user.email = "scott_chief@mars.org"
+        db_sess.add(user)
+
+        user2 = User()
+        user2.surname = "Smith"
+        user2.name = "John"
+        user2.age = 25
+        user2.position = "engineer"
+        user2.speciality = "mechanical engineer"
+        user2.address = "module_2"
+        user2.email = "john_smith@mars.org"
+        db_sess.add(user2)
+
+        user3 = User()
+        user3.surname = "Johnson"
+        user3.name = "Jane"
+        user3.age = 28
+        user3.position = "scientist"
+        user3.speciality = "biologist"
+        user3.address = "module_3"
+        user3.email = "jane_johnson@mars.org"
+        db_sess.add(user3)
+
+        user4 = User()
+        user4.surname = "Brown"
+        user4.name = "Bob"
+        user4.age = 30
+        user4.position = "pilot"
+        user4.speciality = "aeronautics"
+        user4.address = "module_4"
+        user4.email = "bob_brown@mars.org"
+        db_sess.add(user4)
+
+    if db_sess.query(Jobs).count() == 0:
+        job = Jobs()
+        job.team_leader = 1
+        job.job = "deployment of residential modules 1 and 2"
+        job.work_size = 15
+        job.collaborators = "2, 3"
+        job.start_date = datetime.datetime.now()
+        job.is_finished = False
+        db_sess.add(job)
+
+        job2 = Jobs()
+        job2.team_leader = 2
+        job2.job = "maintenance of module 3"
+        job2.work_size = 10
+        job2.collaborators = "1, 3"
+        job2.start_date = datetime.datetime.now()
+        job2.is_finished = True
+        db_sess.add(job2)
+
+    db_sess.commit()
+
+    app.run()
+
+
+@app.route("/")
 def index():
-    return render_template()
+    db_sess = db_session.create_session()
+    data = []
+    for job in db_sess.query(Jobs).all():
+        leader = db_sess.query(User).filter(User.id == job.team_leader).first()
+        data.append((job, leader))
+    return render_template("journal.html", data=data)
+
 
 if __name__ == "__main__":
     main()
