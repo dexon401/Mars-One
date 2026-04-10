7e4dbe32615ff373c2111d810e5a1060ab4182f0
dexon401 <255047233+dexon401@users.noreply.github.com>
2026-04-10 15:01:18 +0300

    added an api returning one job

--- data/jobs_api.py
+++ data/jobs_api.py
@@ -3,14 +3,20 @@ import flask
 from . import db_session
 from .jobs import Jobs
 
-blueprint = flask.Blueprint(
-    "jobs_api",
-    __name__,
-    template_folder="templates"
-)
+blueprint = flask.Blueprint("jobs_api", __name__, template_folder="templates")
+
 
 @blueprint.route("/api/jobs")
 def get_jobs():
     db_sess = db_session.create_session()
     jobs = db_sess.query(Jobs).all()
     return flask.jsonify([item.to_dict(rules=("-user",)) for item in jobs])
+
+
+@blueprint.route("/api/jobs/<int:job_id>")
+def get_job(job_id):
+    db_sess = db_session.create_session()
+    job = db_sess.get(Jobs, job_id)
+    if job:
+        return flask.jsonify(job.to_dict(rules=("-user",)))
+    return "Job not found"
